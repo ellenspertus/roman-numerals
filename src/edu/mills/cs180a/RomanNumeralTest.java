7c51769cec8c3397a0a1a139c88843a28a6a80f6
Irene Serrano <iserrano@mills.edu>
2020-09-22 16:08:37 -0700

    Add 3 tests, Modify existing

--- src/edu/mills/cs180a/RomanNumeralTest.java
+++ src/edu/mills/cs180a/RomanNumeralTest.java
@@ -1,6 +1,7 @@
 package edu.mills.cs180a;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -29,7 +30,29 @@ class RomanNumeralTest {
   @Test
   public void equals_True_Symmetric_X_VV() {
     RomanNumeral rn2 = new RomanNumeral("VV");
-    assertEquals(rnX, rn2);
+    assertEquals(rnX,rn2);
+    assertEquals(rn2,rnX);
+  }
+
+  @Test
+  public void equals_True_Transitive_X_VV_VIIIII() {
+    RomanNumeral rn2 = new RomanNumeral("VV");
+    RomanNumeral rn3 = new RomanNumeral("VIIIII");
+    assertEquals(rnX,rn2);
+    assertEquals(rn2,rn3);
+    assertEquals(rnX,rn3);
+  }
+
+  @Test
+  public void equals_True_Consistent_X_VIIIII() {
+    RomanNumeral rn3 = new RomanNumeral("VIIIII");
+    assertEquals(rnX,rn3);
+  }
+
+  @Test
+  public void equals_False_Null() {
+    RomanNumeral rn2 = new RomanNumeral(null);
+    assertFalse(rnX,rn2);
   }
 
 }
