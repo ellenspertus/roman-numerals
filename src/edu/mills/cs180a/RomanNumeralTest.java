b92f098db266043e04eb170092e4b33b7a82dedf
BHamrick <bhamrick@mills.edu>
2020-09-22 16:09:27 -0700

    Add more tests

--- src/edu/mills/cs180a/RomanNumeralTest.java
+++ src/edu/mills/cs180a/RomanNumeralTest.java
@@ -2,6 +2,7 @@ package edu.mills.cs180a;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -38,5 +39,31 @@ class RomanNumeralTest {
     RomanNumeral z = new RomanNumeral("IV");
 
     assertTrue(x.equals(y) | y.equals(z) | x.equals(z));
+  }
+
+  @Test
+  public void equals_IsSymmetric_BHamrick() {
+    RomanNumeral x = new RomanNumeral("IV");
+    RomanNumeral y = new RomanNumeral("IV");
+
+    assertTrue(x.equals(y) | y.equals(x));
+  }
+
+  @Test
+  public void equals_IsStableOverTime_BHamrick() {
+    RomanNumeral x = new RomanNumeral("X");
+    assertTrue(x.equals(rnX));
+    try {
+      Thread.sleep(1000);
+      assertTrue(x.equals(rnX));
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void equals_IsNotNull_Bhamrick() {
+    RomanNumeral x = new RomanNumeral("IV");
+    assertFalse(x.equals(null));
   }
 }
